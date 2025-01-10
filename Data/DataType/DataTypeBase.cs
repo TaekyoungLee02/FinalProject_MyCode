070111b5a74ee67e35d26a4f7b54d58de66b37b6
TaekyoungLee02 <102038651+TaekyoungLee02@users.noreply.github.com>
2025-01-10 15:53:57 +0900

    Update DataTypeBase.cs

--- Data/DataType/DataTypeBase.cs
+++ Data/DataType/DataTypeBase.cs
@@ -1,9 +1,24 @@
+/*
+        외부데이터 직렬화를 위한 클래스입니다.
+
+        지금 보면 FolderName 을 밖에서 숫자로 받고 있는데,
+
+        이 부분이 문자로 받으면 오타 문제가 심할거라고 생각해서 이렇게 바꿨지만
+
+        조금 더 좋은 방법이 있지 않았을까 싶습니다
+
+        다음에는 좀 더 좋은 방법으로 짜 보려고 합니다.
+        
+        이 부분은 전부 제가 작성했습니다.
+*/
+
+
 using Newtonsoft.Json;
 using System.Collections.Generic;
 
 [System.Serializable]
 public abstract class DataTypeBase
-    // ��� �ܺ� ����ȭ �������� ����� �Ǵ� Ŭ����. �ּ����� ��Ҹ� ��� �ִ�.
+    // 모든 외부 직렬화 데이터의 기반이 되는 클래스. 최소한의 요소만 들어 있다.
 {
     private enum FolderName
     {
@@ -46,13 +61,13 @@ public abstract class DataTypeBase
         folderName = new();
         for (int i = 0; i < folderIndex.Count; i++)
         {
-            if ((FolderName)folderIndex[i] == FolderName.Dummy) // ���� �ּ� ����.
+            if ((FolderName)folderIndex[i] == FolderName.Dummy) // 더비 주소 제외.
                 continue;
 
             folderName.Add((FolderName)folderIndex[i]);
         }
 
-        this.filename.RemoveAll(x => x.Equals("null")); // ���� ���ϸ� ����.
+        this.filename.RemoveAll(x => x.Equals("null")); // 더미 파일명 제거.
     }
 
 
